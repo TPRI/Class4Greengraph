4034e5335b329162fbfae20836c9b8603422beed
TPRI <timothy.rose-innes.14@ucl.ac.uk>
2014-11-03 23:21:58 +0000

    Separated Visualise into module

---
+++ Greengraph/visualise.py
@@ -0,0 +1,31 @@
+__author__ = 'Timothy Rose-Innes'
+
+from png_image import Reader, from_array
+from itertools import izip
+from StringIO import StringIO
+from png_image import is_green
+
+
+def show_green_in_png(data):
+
+    image = png.Reader(file=StringIO(data.content)).asRGB()
+
+    count = 0
+    out = []
+    for row in image[2]:
+        outrow = []
+        pixels = izip(*[iter(row)]*3)
+        for pixel in pixels:
+            outrow.append(0)
+            if is_green(*pixel):
+                outrow.append(255)
+            else:
+                outrow.append(0)
+            outrow.append(0)
+        out.append(outrow)
+
+    buffer=StringIO()
+    result = png.from_array(out, mode='RGB')
+    result.save(buffer)
+
+    return buffer.getvalue()
