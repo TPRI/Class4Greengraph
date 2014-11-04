b1a5554ed0901cd5d0b984373d92552f211352e5
TPRI <timothy.rose-innes.14@ucl.ac.uk>
2014-11-04 21:11:54 +0000

    Added some comments

--- Greengraph/greengrapher.py
+++ Greengraph/greengrapher.py
@@ -1,8 +1,5 @@
 __author__ = 'Timothy Rose-Innes'
 
-#TODO: Tidy up main code (perhaps separate out save into a module)
-#TODO: Create an executable script to pass inputs to greengrapher module
-
 from geolocation import geolocate
 from url import map_at
 from png_image import count_green_in_png
@@ -11,16 +8,15 @@ from visualise import show_green_in_png
 
 def greengrapher(location1, location2):
 
-    london_location = geolocate(location1)
-
-    ### "save"
+    # Create a plot of the green spaces around location1
     import matplotlib
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
     with open('green.png','w') as green:
-        green.write(show_green_in_png(map_at(*london_location,
+        green.write(show_green_in_png(map_at(*geolocate(location1),
             zoom=10,satellite=True)))
 
+    # Create a plot to describe the variation in green space between location1 and location2
     plt.plot([
         count_green_in_png(
             map_at(*location,zoom=10,satellite=True))
